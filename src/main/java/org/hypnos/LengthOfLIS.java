98c607b84696a22a4d8b1e26f04031830b87d061
yuanrui01 <xdyrfree@gmail.com>
2024-05-06 02:23:48 +0800

    递推写法

--- src/main/java/org/hypnos/LengthOfLIS.java
+++ src/main/java/org/hypnos/LengthOfLIS.java
@@ -1,32 +1,22 @@
 package org.hypnos;
 
 /**
- * 300. 最长递增子序列
+ * 300. 最长递增子序列（递推）
  */
 public class LengthOfLIS {
 
-	private int[] nums, memo;
 
 	public int lengthOfLIS(int[] nums) {
-		this.nums = nums;
-		int n = nums.length;
-		memo = new int[n]; // 本题可以初始化成 0，表示没有计算过
-		int ans = 0;
+		int n = nums.length, ans = 0;
+		int[] f = new int[n];
 		for (int i = 0; i < n; i++) {
-			ans = Math.max(ans, dfs(i));
+			for (int j = 0; j < i; j++) {
+				if (nums[j] < nums[i]) {
+					f[i] = Math.max(f[i], f[j]);
+				}
+			}
+			ans = Math.max(ans, ++f[i]);
 		}
 		return ans;
-	}
-
-	private int dfs(int i) {
-		if (memo[i] > 0) { // 之前计算过
-			return memo[i];
-		}
-		for (int j = 0; j < i; j++) {
-			if (nums[j] < nums[i]) {
-				memo[i] = Math.max(memo[i], dfs(j));
-			}
-		}
-		return ++memo[i];
 	}
 }
