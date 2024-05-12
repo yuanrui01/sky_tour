f47d1588d62160c14d211f6ecb195d6161c9aacd
yuanrui <xdyrfree@gmail.com>
2024-05-12 19:16:28 +0800

    1283. 使结果不超过阈值的最小除数

---
+++ src/main/java/org/hypnos/SmallestDivisor.java
@@ -0,0 +1,48 @@
+package org.hypnos;
+
+
+import java.util.Arrays;
+
+/**
+ * 1283. 使结果不超过阈值的最小除数
+ */
+public class SmallestDivisor {
+
+    private int[] nums;
+
+    public int smallestDivisor(int[] nums, int threshold) {
+        Arrays.sort(nums);
+        this.nums = nums;
+        int left = 1;
+        int right = nums[nums.length - 1];
+
+        while ( left <= right ) {
+            int mid = left + (right - left) / 2;
+            long sum = getSum(mid);
+            if (sum > threshold)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+
+    private long getSum(int divisor) {
+        long ans = 0;
+        for (int num : nums) {
+            ans += num / divisor;
+            if (num % divisor != 0)
+                ans++;
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        SmallestDivisor smallestDivisor = new SmallestDivisor();
+        int[] nums = {2,3,5,7,11};
+        int threshold = 11;
+        System.out.println(smallestDivisor.smallestDivisor(nums, threshold));
+    }
+}
