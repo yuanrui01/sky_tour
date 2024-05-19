e380a369a81d6565e171d795cfcda368b3a406bf
yuanrui <xdyrfree@gmail.com>
2024-05-19 15:48:32 +0800

    LCR 166. 珠宝的最高价值 - 空间优化

--- src/main/java/org/hypnos/JewelleryValue.java
+++ src/main/java/org/hypnos/JewelleryValue.java
@@ -2,20 +2,20 @@ package org.hypnos;
 
 
 /**
- * LCR 166. 珠宝的最高价值
+ * LCR 166. 珠宝的最高价值 - 空间优化
  */
 public class JewelleryValue {
 
     public int jewelleryValue(int[][] frame) {
         int m = frame.length;
         int n = frame[0].length;
-        int[][] memo = new int[m+1][n+1];
+        int[] memo = new int[n + 1];
         for (int i = 1; i < m + 1; ++i) {
             for (int j = 1; j < n + 1; ++j) {
-                memo[i][j] = Math.max(memo[i-1][j], memo[i][j-1]) + frame[i-1][j-1];
+                memo[j] = Math.max(memo[j], memo[j - 1]) + frame[i - 1][j - 1];
             }
         }
-        return memo[m][n];
+        return memo[n];
     }
 
     public static void main(String[] args) {
