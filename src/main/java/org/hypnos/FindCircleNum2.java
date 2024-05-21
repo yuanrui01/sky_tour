c04740ed5b9254c3c9f8044f01198fa35f16b427
yuanrui <xdyrfree@gmail.com>
2024-05-21 11:13:27 +0800

    547. 省份数量 - 并查集-路径压缩写法

---
+++ src/main/java/org/hypnos/FindCircleNum2.java
@@ -0,0 +1,39 @@
+package org.hypnos;
+
+/**
+ * 547. 省份数量 - 并查集-路径压缩写法
+ */
+public class FindCircleNum2 {
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int[] parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; ++j) {
+               if (isConnected[i][j] == 1) {
+                   union(parents, i, j);
+               }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (parents[i] == i)
+                ans++;
+        }
+        return ans;
+    }
+
+    private void union(int[] parents, int i, int j) {
+        parents[find(parents, i)] = parents[find(parents, j)];
+    }
+
+    private int find(int[] parents, int i) {
+        if (parents[i] != i) {
+            parents[i] = find(parents, parents[i]);
+        }
+        return parents[i];
+    }
+}
