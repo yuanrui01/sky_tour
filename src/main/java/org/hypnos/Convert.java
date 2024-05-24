6324916abd5240ae1d0218e8a82c8cf3e41d0dde
yuanrui <xdyrfree@gmail.com>
2024-05-24 10:54:04 +0800

    1311. 获取你好友已观看的视频 - BFS

--- src/main/java/org/hypnos/Convert.java
+++ src/main/java/org/hypnos/Convert.java
@@ -1,8 +1,6 @@
 package org.hypnos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 /**
--- src/main/java/org/hypnos/WatchedVideosByFriends.java
+++ src/main/java/org/hypnos/WatchedVideosByFriends.java
@@ -4,41 +4,29 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * 1311. 获取你好友已观看的视频
+ * 1311. 获取你好友已观看的视频 - BFS
  */
 public class WatchedVideosByFriends {
-
-	private class Node {
-		public String video;
-		public int count;
-
-
-		public Node(String video, int count) {
-			this.video = video;
-			this.count = count;
-		}
-
-		public String getVideo() {
-			return video;
-		}
-
-		public int getCount() {
-			return count;
-		}
-	}
-
 
 	public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
 		int n = friends.length;
 		boolean[] vis = new boolean[n];
 		Queue<Integer> queue = new ArrayDeque<>();
-		int k = 0;
+		int k = -1;
 		vis[id] = true;
 		queue.add(id);
 		while (!queue.isEmpty()) {
 			List<Integer> list = new ArrayList<>();
 			while (!queue.isEmpty()) {
 				list.add(queue.poll());
+			}
+			for (Integer i : list) {
+				for (int friend : friends[i]) {
+					if (!vis[friend]) {
+						vis[friend] = true;
+						queue.add(friend);
+					}
+				}
 			}
 			k++;
 			if (k == level) {
@@ -48,17 +36,12 @@ public class WatchedVideosByFriends {
 						result.merge(video, 1, Integer::sum);
 					}
 				}
-				List<Node> nodes = result.entrySet().stream().map(entry -> new Node(entry.getKey(), entry.getValue())).collect(Collectors.toList());
-				nodes.sort(Comparator.comparingInt(Node::getCount).thenComparing(Node::getVideo));
-				return nodes.stream().map(Node::getVideo).collect(Collectors.toList());
-			}
-			for (Integer i : list) {
-				for (int friend : friends[i]) {
-					if (!vis[friend]) {
-						vis[friend] = true;
-						queue.add(friend);
-					}
-				}
+				return result.entrySet().stream().sorted((o1, o2) -> {
+                    if (o1.getValue() - o2.getValue() == 0)
+                        return o1.getKey().compareTo(o2.getKey());
+                    else
+                        return o1.getValue() - o2.getValue();
+                }).map(Map.Entry::getKey).collect(Collectors.toList());
 			}
 		}
 		return null;
