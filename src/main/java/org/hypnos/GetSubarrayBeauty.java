7ee1a6265ea4617f01dd7e0834d32f722dedfb4a
yuanrui <xdyrfree@gmail.com>
2024-05-28 11:58:47 +0800

    2653. 滑动子数组的美丽值 - 滑动窗口 + 暴力枚举

--- src/main/java/org/hypnos/GetSubarrayBeauty.java
+++ src/main/java/org/hypnos/GetSubarrayBeauty.java
@@ -4,45 +4,50 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
- * 2653. 滑动子数组的美丽值
+ * 2653. 滑动子数组的美丽值 - 滑动窗口 + 暴力枚举
  */
 public class GetSubarrayBeauty {
+
+
 	public int[] getSubarrayBeauty(int[] nums, int k, int x) {
 		int len = nums.length;
-		int[] result = new int[len - k + 1];
-		TreeSet<Integer> set = new TreeSet<>();
+		int[] ans = new int[len - k + 1];
+		int[] forces = new int[101];
 
 		for (int i = 0; i < k; ++i) {
-			if (set.size() != x)
-				set.add(nums[i]);
-			else if (nums[i] < set.last()) {
-				set.pollLast();
-				set.add(nums[i]);
+			forces[nums[i] + 50]++;
+		}
+		for (int i = 0, ix = 0; i < 50; ++i) {
+			if (forces[i] > 0) {
+				ix += forces[i];
+				if (ix >= x) {
+					ans[0] = i - 50;
+					break;
+				}
 			}
 		}
-		if (set.last() < 0)
-			result[0] = set.last();
 
-		for (int i = k; i < len; ++i) {
-			set.remove(nums[i - k]);
-			if (set.size() != x)
-				set.add(nums[i]);
-			else if (nums[i] < set.last()) {
-				set.pollLast();
-				set.add(nums[i]);
+		for (int i = k; i < nums.length; ++i) {
+			forces[nums[i] + 50]++;
+			forces[nums[i - k] + 50]--;
+			for (int j = 0, ij = 0; j < 50; ++j) {
+				if (forces[j] > 0) {
+					ij += forces[j];
+					if (ij >= x) {
+						ans[i - k + 1] = j - 50;
+						break;
+					}
+				}
 			}
-			set.
-			if (set.last() < 0)
-				result[i - k + 1] = set.last();
 		}
-		return result;
+		return ans;
 	}
 
 
 	public static void main(String[] args) {
-		int[] nums = new int[]{-30,-17,23,35};
+		int[] nums = new int[]{1,-1,-3,-2,3};
 		int k = 3;
-		int x = 1;
+		int x = 2;
 
 		int[] subarrayBeauty = new GetSubarrayBeauty().getSubarrayBeauty(nums, k, x);
 		System.out.println(Arrays.toString(subarrayBeauty));
