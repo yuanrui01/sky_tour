804b0dbfac7ce4c8dd809f4ee5032e56f170158e
yuanrui01 <xdyrfree@gmail.com>
2024-06-01 00:21:52 +0800

    2928. 给小朋友们分糖果 I

---
+++ src/main/java/org/hypnos/DistributeCandies.java
@@ -0,0 +1,20 @@
+package org.hypnos;
+
+/**
+ * 2928. 给小朋友们分糖果 I
+ */
+public class DistributeCandies {
+
+	public int distributeCandies(int n, int limit) {
+		int ans = 0;
+		for (int i = 0; i <= limit; ++i) {
+			for (int j = 0; j <= limit; ++j) {
+				for (int k = 0; k <= limit; ++k) {
+					if (i + j + k == n)
+						ans++;
+				}
+			}
+		}
+		return ans;
+	}
+}
