9a2971b4dcaa040cd8e4f6bb6f632118b1e6e5b5
yuanrui01 <xdyrfree@gmail.com>
2024-06-02 15:43:43 +0800

    100315. 找到按位与最接近 K 的子数组

---
+++ src/main/java/org/hypnos/MinimumDifference.java
@@ -0,0 +1,20 @@
+package org.hypnos;
+
+/**
+ * 100315. 找到按位与最接近 K 的子数组
+ */
+public class MinimumDifference {
+
+	public int minimumDifference(int[] nums, int k) {
+		int ans = Integer.MAX_VALUE;
+		for (int i = 0; i < nums.length; ++i) {
+			int x = nums[i];
+			ans = Math.min(ans, Math.abs(x - k));
+			for (int j = i - 1; j >= 0 && (nums[j] & x) != nums[j] ; --j) {
+				nums[j] &= x;
+				ans = Math.min(ans, Math.abs(nums[j] - k));
+			}
+		}
+		return ans;
+	}
+}
