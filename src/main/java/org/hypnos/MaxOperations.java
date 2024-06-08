05141d66c6e63627944d4ff6ad10ed6ccabbf61b
yuanrui01 <xdyrfree@gmail.com>
2024-06-08 22:02:50 +0800

    3040. 相同分数的最大操作数目 II

--- src/main/java/org/hypnos/MaxOperations.java
+++ src/main/java/org/hypnos/MaxOperations.java
@@ -1,7 +1,6 @@
 package org.hypnos;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 /**
  * 3040. 相同分数的最大操作数目 II
@@ -11,34 +10,41 @@ public class MaxOperations {
 	public int maxOperations(int[] nums) {
 		int left = 0;
 		int right = nums.length - 1;
-		Map<String,Integer> cache = new HashMap<>();
 
-		int res1 =  dp(nums, cache,left + 1, right - 1, nums[left] + nums[right]);
-		int res2 = dp(nums, cache,left + 2, right, nums[left] + nums[left + 1]);
-		int res3 = dp(nums, cache,left, right - 2, nums[right - 1] + nums[right]);
+		int res1 =  helper(nums,left + 1, right - 1, nums[left] + nums[right]);
+		int res2 = helper(nums,left + 2, right, nums[left] + nums[left + 1]);
+		int res3 = helper(nums,left, right - 2, nums[right - 1] + nums[right]);
 		return Math.max(res3, Math.max(res1, res2)) + 1;
 	}
 
-	private int dp(int[] nums, Map<String,Integer> cache, int left, int right, int target) {
+	private int helper(int[] nums, int left, int right, int target) {
+		int n = nums.length;
+		int[][] memo = new int[n][n];
+		for (int[] ints : memo) {
+			Arrays.fill(ints, -1);
+		}
+		return dp(nums, memo, left, right, target);
+	}
+
+	private int dp(int[] nums, int[][] memo, int left, int right, int target) {
 		if (left >= right)
 			return 0;
-		String cacheKey = left + "," + right + "," + target;
-		if (cache.get(cacheKey) != null)
-			return cache.get(cacheKey);
+		if (memo[left][right] != -1)
+			return memo[left][right];
 		int res1 = 0;
 		if (target == nums[left] + nums[right]) {
-			res1 = dp(nums, cache, left + 1, right - 1, target) + 1;
+			res1 = dp(nums, memo, left + 1, right - 1, target) + 1;
 		}
 		int res2 = 0;
 		if (target == nums[left] + nums[left + 1]) {
-			res2 = dp(nums, cache, left + 2, right, nums[left] + nums[left + 1]) + 1;
+			res2 = dp(nums, memo, left + 2, right, nums[left] + nums[left + 1]) + 1;
 		}
 		int res3 = 0;
 		if (target == nums[right - 1] + nums[right]) {
-			res3 = dp(nums, cache,  left, right - 2, nums[right - 1] + nums[right]) + 1;
+			res3 = dp(nums, memo,  left, right - 2, nums[right - 1] + nums[right]) + 1;
 		}
 		int res = Math.max(res3, Math.max(res1, res2));
-		cache.put(cacheKey, res);
+		memo[left][right] = res;
 		return res;
 	}
 
