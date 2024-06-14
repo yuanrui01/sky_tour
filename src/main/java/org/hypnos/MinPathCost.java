61a5b601648491dbb49c3b463018436713b1ffbd
yuanrui <xdyrfree@gmail.com>
2024-06-14 16:55:05 +0800

    补充递推写法

--- src/main/java/org/hypnos/MinPathCost.java
+++ src/main/java/org/hypnos/MinPathCost.java
@@ -39,6 +39,27 @@ public class MinPathCost {
 		return ans;
 	}
 
+
+	public int minPathCost2(int[][] grid, int[][] moveCost) {
+		int m = grid.length;
+		int n = grid[0].length;
+
+		for (int i = m - 2; i >= 0; --i) {
+			for (int j = 0; j < n; ++j) {
+				int res = Integer.MAX_VALUE;
+				for (int k = 0; k < n; ++k) {
+					res = Math.min(res, moveCost[grid[i][j]][k] + grid[i + 1][k]);
+				}
+				grid[i][j] += res;
+			}
+		}
+
+		int ans = Integer.MAX_VALUE;
+		for (int num : grid[0])
+			ans = Math.min(ans, num);
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		int[][] grid = {{5,3},{4,0},{2,1}};
 		int[][] moveCost = {{9,8},{1,5},{10,12},{18,6},{2,4},{14,3}};
