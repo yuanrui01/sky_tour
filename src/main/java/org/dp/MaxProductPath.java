45a27f91ff30ceddca4b9c8459a2a520a13c1b13
yuanrui01 <xdyrfree@gmail.com>
2024-06-15 00:25:01 +0800

    1594. 矩阵的最大非负积 dfs

--- src/main/java/org/dp/MaxProductPath.java
+++ src/main/java/org/dp/MaxProductPath.java
@@ -1,7 +1,5 @@
 package org.dp;
 
-import java.math.BigInteger;
-
 /**
  * 1594. 矩阵的最大非负积
  */
@@ -11,33 +9,31 @@ public class MaxProductPath {
 
 	private int m;
 	private int n;
-	private BigInteger ans;
+	private long ans;
 	public int maxProductPath(int[][] grid) {
 		this.m = grid.length;
 		this.n = grid[0].length;
-		this.ans = BigInteger.valueOf(-1L);
+		this.ans = -1;
 		boolean[][] vis = new boolean[m][n];
-		dfs(grid, vis, BigInteger.valueOf(1), 0, 0);
-		return ans.compareTo(BigInteger.ZERO) < 0 ? -1 : ans.mod(BigInteger.valueOf(1000000007L)).intValue();
+		dfs(grid, vis, 1L, 0, 0);
+		return (int) (ans % LIMIT);
 	}
 
-
-	private void dfs(int[][] grid, boolean[][] vis, BigInteger pre, int i, int j) {
-		if (pre.equals(BigInteger.ZERO)) {
-			ans = ans.max(BigInteger.ZERO);
+	private void dfs(int[][] grid, boolean[][] vis, long pre, int i, int j) {
+		if (pre == 0) {
+			ans = Math.max(ans, 0);
 			return;
 		}
-		BigInteger multiply = pre.multiply(BigInteger.valueOf(grid[i][j]));
+		long tmp = pre * grid[i][j];
 		if (i == m - 1 && j == n - 1) {
-			ans = ans.max(multiply);
-			return;
+			ans = Math.max(ans, tmp);
+			return ;
 		}
-
 		vis[i][j] = true;
 		if (i != m - 1 && !vis[i + 1][j])
-			dfs(grid, vis, multiply, i + 1, j);
+			dfs(grid, vis, tmp, i + 1, j);
 		if (j < n - 1 && !vis[i][j + 1])
-			dfs(grid, vis, multiply, i, j + 1);
+			dfs(grid, vis, tmp, i, j + 1);
 		vis[i][j] = false;
 	}
 
