36b7974abf0e4f65d7b1b54e54875cad4a05027f
yuanrui <xdyrfree@gmail.com>
2024-06-26 14:23:29 +0800

    2267. 检查是否有合法括号字符串路径

--- src/main/java/org/dp/HasValidPath.java
+++ src/main/java/org/dp/HasValidPath.java
@@ -9,17 +9,15 @@ public class HasValidPath {
     private int m;
     private int n;
     private char[][] grid;
-    private boolean[][][] dp;
+    private boolean[][][] vis;
 
     public boolean hasValidPath(char[][] grid) {
         this.m = grid.length;
         this.n = grid[0].length;
         this.grid = grid;
-
         if ((m + n - 1) % 2 != 0 || grid[0][0] == ')' || grid[m - 1][n - 1] == '(')
             return false;
-        this.dp = new boolean[m][n][(m + n + 1) / 2];
-
+        this.vis = new boolean[m][n][(m + n + 1) / 2];
         return dfs(0, 0, 0);
     }
 
@@ -28,9 +26,9 @@ public class HasValidPath {
             return false;
         if (i == m - 1 && j == n - 1)
             return c == 1;
-        if (dp[i][j][c])
+        if (vis[i][j][c]) // 访问过还在跑就是失败的
             return false;
-        dp[i][j][c] = true;
+        vis[i][j][c] = true;
         c += grid[i][j] == ')' ? -1 : 1;
         return c >= 0 && (i < m - 1 && dfs(i + 1, j, c) || j < n - 1 && dfs(i, j + 1, c));
     }
