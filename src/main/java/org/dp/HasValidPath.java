92efea4c72b33bd2a08c4e9f03b22a1e2d560854
yuanrui <xdyrfree@gmail.com>
2024-06-26 14:21:08 +0800

    2267. 检查是否有合法括号字符串路径

---
+++ src/main/java/org/dp/HasValidPath.java
@@ -0,0 +1,37 @@
+package org.dp;
+
+
+/**
+ * 2267. 检查是否有合法括号字符串路径
+ */
+public class HasValidPath {
+
+    private int m;
+    private int n;
+    private char[][] grid;
+    private boolean[][][] dp;
+
+    public boolean hasValidPath(char[][] grid) {
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.grid = grid;
+
+        if ((m + n - 1) % 2 != 0 || grid[0][0] == ')' || grid[m - 1][n - 1] == '(')
+            return false;
+        this.dp = new boolean[m][n][(m + n + 1) / 2];
+
+        return dfs(0, 0, 0);
+    }
+
+    private boolean dfs(int i, int j, int c) {
+        if (c > m - i + n - j - 1)
+            return false;
+        if (i == m - 1 && j == n - 1)
+            return c == 1;
+        if (dp[i][j][c])
+            return false;
+        dp[i][j][c] = true;
+        c += grid[i][j] == ')' ? -1 : 1;
+        return c >= 0 && (i < m - 1 && dfs(i + 1, j, c) || j < n - 1 && dfs(i, j + 1, c));
+    }
+}
