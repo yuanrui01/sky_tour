2e79a2a9667de887c9b4e8d361a9a4a37cea473a
yuanrui <xdyrfree@gmail.com>
2024-07-30 14:50:38 +0800

    2658. 网格图中鱼的最大数目

---
+++ src/main/java/org/grid/FindMaxFish.java
@@ -0,0 +1,40 @@
+package org.grid;
+
+
+/**
+ * 2658. 网格图中鱼的最大数目
+ */
+public class FindMaxFish {
+
+    private static final int[][] dirs = {{0,1}, {1,0}, {0, -1}, {-1,0}};
+
+
+    public int findMaxFish(int[][] grid) {
+        int ans = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] vis = new boolean[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] != 0 && !vis[i][j]) {
+                    ans = Math.max(ans, dfs(grid, vis, m, n, i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    private int dfs(int[][] grid, boolean[][] vis, int m, int n, int i, int j) {
+        int res = grid[i][j];
+        vis[i][j] = true;
+        for (int[] dir : dirs) {
+            int newX = i + dir[0];
+            int newY = j + dir[1];
+            if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] != 0 && !vis[newX][newY]) {
+                res += dfs(grid, vis, m, n, newX, newY);
+            }
+        }
+        return res;
+    }
+}
