2ec8b86d8d0bae4ff8ce85945acc6ee369476b83
yuanrui01 <xdyrfree@gmail.com>
2024-08-13 00:26:35 +0800

    3151. 特殊数组 I

---
+++ src/main/java/org/hypnos/IsArraySpecial.java
@@ -0,0 +1,17 @@
+package org.hypnos;
+
+/**
+ * 3151. 特殊数组 I
+ */
+public class IsArraySpecial {
+
+	public boolean isArraySpecial(int[] nums) {
+		if (nums.length == 1)
+			return true;
+		for (int i = 0; i < nums.length - 1; ++i) {
+			if (nums[i] % 2 == nums[i + 1] % 2)
+				return false;
+		}
+		return true;
+	}
+}
