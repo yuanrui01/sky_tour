a224e41fac69538e96268e8276363d44772dce8e
yuanrui <xdyrfree@gmail.com>
2024-08-26 17:11:25 +0800

    滑窗复习

--- src/main/java/org/hypnos/MaxSum.java
+++ src/main/java/org/hypnos/MaxSum.java
@@ -12,47 +12,35 @@ public class MaxSum {
 
     public long maxSum(List<Integer> nums, int m, int k) {
         long ans = 0;
-        Map<Integer, Integer> countMap = new HashMap<>();
-        long tSum = 0;
-        int differentCount = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = nums.stream().mapToInt(i -> i).toArray();
+        long kSum = 0;
         for (int i = 0; i < k; ++i) {
-            Integer iNum = nums.get(i);
-            tSum += iNum;
-            //===========================
-            Integer iNumCount = countMap.getOrDefault(iNum, 0);
-            if (iNumCount == 0)
-                differentCount++;
-            countMap.merge(iNum, 1, Integer::sum);
+            kSum += arr[i];
+            map.merge(arr[i], 1, Integer::sum);
         }
-        //===========================
-        if (differentCount >= m)
-            ans = tSum;
-        //===========================
-        for (int i = k; i < nums.size(); ++i) {
-            Integer iNum = nums.get(i);
-            tSum += iNum;
-            tSum -= nums.get(i - k);
-            //===========================
-            Integer iNumCount = countMap.getOrDefault(iNum, 0);
-            if (iNumCount == 0)
-                differentCount++;
-            countMap.merge(iNum, 1, Integer::sum);
-            //===========================
-            Integer preKCount = countMap.get(nums.get(i - k));
-            countMap.put(nums.get(i - k), --preKCount);
-            if (preKCount == 0)
-                differentCount--;
-            //===========================
-            if (differentCount >= m)
-                ans = Math.max(ans, tSum);
+        if (map.size() >= m) {
+            ans = Math.max(ans, kSum);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            kSum -= arr[i - k];
+            kSum += arr[i];
+            map.merge(arr[i], 1, Integer::sum);
+            Integer ik = map.get(arr[i - k]);
+            if (ik == 1)
+                map.remove(arr[i-k]);
+            else
+                map.put(arr[i-k], ik - 1);
+            if (map.size() >= m)
+                ans = Math.max(ans, kSum);
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        List<Integer> nums = List.of(1,1,2);
-        int m = 1;
-        int k = 1;
+        List<Integer> nums = List.of(1,2,1,2,1,2,1);
+        int m = 3;
+        int k = 3;
 
         MaxSum maxSum = new MaxSum();
         System.out.println(maxSum.maxSum(nums,m,k));
