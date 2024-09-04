fbd52725ff7518b96c00ac6b2d602805f6f59b74
yuanrui <xdyrfree@gmail.com>
2024-09-04 10:00:33 +0800

    2860. 让所有学生保持开心的分组方法数

---
+++ src/main/java/org/slide/CountWays.java
@@ -0,0 +1,24 @@
+package org.slide;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+ * 2860. 让所有学生保持开心的分组方法数
+ */
+public class CountWays {
+
+    public int countWays(List<Integer> nums) {
+        int[] a = nums.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(a);
+        int ans = a[0] > 0 ? 1 : 0;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i - 1] < i && i < a[i]) {
+                ans++;
+            }
+        }
+        return ans + 1;
+    }
+}
