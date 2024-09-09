dcf9583b639624812da9b5b9c8e8426aa6fc6373
yuanrui01 <xdyrfree@gmail.com>
2024-09-09 21:00:46 +0800

    2181. 合并零之间的节点

---
+++ src/main/java/org/common/ListNode.java
@@ -0,0 +1,19 @@
+package org.common;
+
+/**
+ * @author: yuanrui
+ */
+public class ListNode {
+
+	public int val;
+	public ListNode next;
+
+	public ListNode() {}
+	public ListNode(int val) {
+		this.val = val;
+	}
+	public ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
---
+++ src/main/java/org/list/MergeNodes.java
@@ -0,0 +1,31 @@
+package org.list;
+
+import org.common.ListNode;
+
+/**
+ * 2181. 合并零之间的节点
+ * @author yuanrui
+ */
+public class MergeNodes {
+
+	public ListNode mergeNodes(ListNode head) {
+		int sum = 0;
+		ListNode hd = new ListNode();
+		ListNode cur = new ListNode(0);
+		hd.next = cur;
+		while (head != null) {
+			if (head.val != 0) {
+				sum += head.val;
+			} else if (head.next != null) {
+				cur.val = sum;
+				cur.next = new ListNode(0);
+				cur = cur.next;
+				sum = 0;
+			} else {
+				cur.val = sum;
+			}
+			head = head.next;
+		}
+		return hd.next;
+	}
+}
