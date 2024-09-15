7e8fa8195a1d8ae9b6dc508d9fcc1a2ee574c968
yuanrui <xdyrfree@gmail.com>
2024-09-15 10:14:58 +0800

    2848. 与车相交的点

--- src/main/java/org/hypnos/NumberOfPoints.java
+++ src/main/java/org/hypnos/NumberOfPoints.java
@@ -8,22 +8,12 @@ import java.util.*;
 public class NumberOfPoints {
 
     public int numberOfPoints(List<List<Integer>> nums) {
-        List<List<Integer>> merge = merge(nums);
-        int ans = 0;
-        for (List<Integer> integers : merge) {
-            ans += integers.get(1) - integers.get(0);
-        }
-        return ans + merge.size();
-    }
-
-    public List<List<Integer>> merge(List<List<Integer>> nums) {
         if (nums.size() <= 1) {
-            return List.of(nums.get(0));
+            return nums.get(0).get(1) - nums.get(0).get(0) + 1;
         }
+        int ans = 0;
         nums.sort(Comparator.comparingInt(a -> a.get(0)));
-        List<List<Integer>> merged = new ArrayList<>();
         List<Integer> currentInterval = nums.get(0);
-        merged.add(currentInterval);
         for (List<Integer> interval : nums) {
             Integer currentEnd = currentInterval.get(1);
             Integer nextStart = interval.get(0);
@@ -31,10 +21,25 @@ public class NumberOfPoints {
             if (currentEnd >= nextStart) {
                 currentInterval.set(1, Math.max(currentEnd, nextEnd));
             } else {
+                ans += currentEnd - currentInterval.get(0) + 1;
                 currentInterval = interval;
-                merged.add(currentInterval);
             }
         }
-        return merged;
+        ans += currentInterval.get(1) - currentInterval.get(0) + 1;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> trueL = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(3);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(5);
+        list2.add(8);
+        trueL.add(list1);
+        trueL.add(list2);
+        NumberOfPoints numberOfPoints = new NumberOfPoints();
+        System.out.println(numberOfPoints.numberOfPoints(trueL));
     }
 }
