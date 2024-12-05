0df183b505df31e85b8718665d8674c6bf046cd3
yuanrui01 <xdyrfree@gmail.com>
2024-12-06 06:24:15 +0800

    160. 相交链表

--- src/main/java/org/list/GetIntersectionNode.java
+++ src/main/java/org/list/GetIntersectionNode.java
@@ -2,28 +2,18 @@ package org.list;
 
 import org.common.ListNode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 160. 相交链表
  */
 public class GetIntersectionNode {
 
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-		ListNode tmpA = headA;
-		ListNode tmpB = headB;
-		Set<ListNode> set = new HashSet<>();
-		while (tmpA != null) {
-			set.add(tmpA);
-			tmpA = tmpA.next;
+		ListNode pA = headA;
+		ListNode pB = headB;
+		while (pA != pB) {
+			pA = pA == null ? headB : pA.next;
+			pB = pB == null ? headA : pB.next;
 		}
-		while (tmpB != null) {
-			if (set.contains(tmpB)) {
-				return tmpB;
-			}
-			tmpB = tmpB.next;
-		}
-		return null;
+		return pA;
 	}
 }
