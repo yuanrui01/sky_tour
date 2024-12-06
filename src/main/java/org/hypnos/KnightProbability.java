e5b68195768228ca9e2e4545a3ef66e2adb05bab
yuanrui01 <xdyrfree@gmail.com>
2024-12-07 07:19:23 +0800

    688. 骑士在棋盘上的概率

--- src/main/java/org/hypnos/KnightProbability.java
+++ src/main/java/org/hypnos/KnightProbability.java
@@ -1,5 +1,7 @@
 package org.hypnos;
 
+import java.util.Arrays;
+
 /**
  * 688. 骑士在棋盘上的概率
  */
@@ -7,9 +9,16 @@ public class KnightProbability {
 
 	private int n;
 	private int[][] dirs = {{1,-2}, {2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2}};
+	private double[][][] cache;
 
 	public double knightProbability(int n, int k, int row, int column) {
 		this.n = n;
+		cache = new double[n][n][k + 1];
+		for (double[][] cs : cache) {
+			for (double[] c : cs) {
+				Arrays.fill(c, -1);
+			}
+		}
 		return dfs(row, column, k);
 	}
 
@@ -17,6 +26,7 @@ public class KnightProbability {
 		if (k == 0) {
 			return 1;
 		}
+		if (cache[x][y][k] != -1) return cache[x][y][k];
 		double result = 0.0;
 		for (int[] dir : dirs) {
 			int newX = x + dir[0];
@@ -25,12 +35,11 @@ public class KnightProbability {
 				result += dfs(newX, newY, k - 1) * 0.125;
 			}
 		}
-		return result;
+		return cache[x][y][k] = result;
 	}
 
 	public static void main(String[] args) {
 		KnightProbability knightProbability = new KnightProbability();
-		System.out.println(knightProbability.knightProbability(3, 1, 1, 1));
-		System.out.println(knightProbability.dfs(1,2,1));
+		System.out.println(knightProbability.knightProbability(8, 30, 6, 4));
 	}
 }
