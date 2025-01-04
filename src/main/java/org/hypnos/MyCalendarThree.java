54ba58fcc76a519f3c34199d10456e1f7ff3456c
yuanrui <xdyrfree@gmail.com>
2025-01-04 23:38:42 +0800

    732. 我的日程安排表 III

---
+++ src/main/java/org/hypnos/MyCalendarThree.java
@@ -0,0 +1,30 @@
+package org.hypnos;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 732. 我的日程安排表 III
+ * @author: yuanrui
+ */
+public class MyCalendarThree {
+
+	private TreeMap<Integer, Integer> cnt;
+
+	public MyCalendarThree() {
+		cnt = new TreeMap<>();
+	}
+
+	public int book(int start, int end) {
+		int ans = 0;
+		int maxBook = 0;
+		cnt.put(start, cnt.getOrDefault(start, 0) + 1);
+		cnt.put(end, cnt.getOrDefault(end, 0) - 1);
+		for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+			int freq = entry.getValue();
+			maxBook += freq;
+			ans = Math.max(maxBook, ans);
+		}
+		return ans;
+	}
+}
