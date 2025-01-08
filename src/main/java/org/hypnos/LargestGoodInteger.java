3d60b0c8aa7dcc8eb052473d898dfefa4f688fe5
yuanrui <xdyrfree@gmail.com>
2025-01-08 20:56:03 +0800

    2264. 字符串中最大的 3 位相同数字

--- src/main/java/org/hypnos/LargestGoodInteger.java
+++ src/main/java/org/hypnos/LargestGoodInteger.java
@@ -1,22 +1,19 @@
 package org.hypnos;
 
 /**
- * @author: yuanrui
+ * 2264. 字符串中最大的 3 位相同数字
  */
 public class LargestGoodInteger {
 
 	public String largestGoodInteger(String num) {
-		int min = '0' - 1;
-		int ans = min;
+		char ans = 0;
 		int len = num.length();
 		for (int i = 2; i < len; ++i) {
-			if (num.charAt(i) == num.charAt(i-1) && num.charAt(i-1) == num.charAt(i-2)) {
-				ans = Math.max(ans, num.charAt(i));
+			if (num.charAt(i) >  ans && num.charAt(i) == num.charAt(i-1) && num.charAt(i-1) == num.charAt(i-2)) {
+				ans = num.charAt(i);
 			}
 		}
-		if (ans == min) return "";
-		ans = ans - '0';
-		return ans + "" + ans + ans;
+		return ans > 0 ? String.valueOf(ans).repeat(3) : "";
 	}
 
 	public static void main(String[] args) {
