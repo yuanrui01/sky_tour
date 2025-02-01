03edcfe32aa20991a8c1d09be578a8652ecda7b8
yuanrui <xdyrfree@gmail.com>
2025-02-01 10:54:51 +0800

    补充解法

--- src/main/java/org/search/RotateSortingArray.java
+++ src/main/java/org/search/RotateSortingArray.java
@@ -36,6 +36,40 @@ public class RotateSortingArray {
         return -1;
     }
 
+
+    // 找最小的做法
+//    public int search(int[] nums, int target) {
+//        int minIndex = findMin(nums);
+//        int r1 = search(nums, 0, minIndex - 1, target);
+//        if (r1 != -1) return r1;
+//        return search(nums, minIndex, nums.length - 1, target);
+//    }
+//
+//    private int search(int[] nums, int left, int right, int target) {
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] == target) return mid;
+//            else if (nums[mid] < target) left = mid + 1;
+//            else right = mid -1;
+//        }
+//        return -1;
+//    }
+//
+//    private int findMin(int[] nums) {
+//        int left = 0;
+//        int right = nums.length - 1;
+//        int last = nums[right];
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] <= last) {
+//                right = mid - 1;
+//            } else {
+//                left = mid + 1;
+//            }
+//        }
+//        return left;
+//    }
+
     public static void main(String[] args) {
         RotateSortingArray rotateSortingArray = new RotateSortingArray();
         int[] nums = {4,5,6,7,0,1,2};
