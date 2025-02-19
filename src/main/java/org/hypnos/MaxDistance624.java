8a0c26eca7ac357dd0ea59278f21ed8a8d2abebb
yuanrui <xdyrfree@gmail.com>
2025-02-19 10:32:16 +0800

    624. 数组列表中的最大距离

---
+++ src/main/java/org/hypnos/MaxDistance624.java
@@ -0,0 +1,28 @@
+package org.hypnos;
+
+
+import java.util.List;
+
+/**
+ * 624. 数组列表中的最大距离
+ */
+public class MaxDistance624 {
+
+    public int maxDistance(List<List<Integer>> arrays) {
+        int mn = arrays.get(0).get(0);
+        int mx = arrays.get(0).get(arrays.get(0).size()-1);
+        int ans = -1;
+        for (int i = 1; i < arrays.size(); ++i) {
+            List<Integer> array = arrays.get(i);
+            int first  = array.get(0);
+            int last = array.get(array.size()-1);
+            ans = Math.max(ans, Math.abs(mn - first));
+            ans = Math.max(ans, Math.abs(mn - last));
+            ans = Math.max(ans, Math.abs(mx - first));
+            ans = Math.max(ans, Math.abs(mx - last));
+            mn = Math.min(mn, first);
+            mx = Math.max(mx, last);
+        }
+        return ans;
+    }
+}
