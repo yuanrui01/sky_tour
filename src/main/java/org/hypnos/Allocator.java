0b4a4113b22300087fcd260e533f33448f437b5e
yuanrui <xdyrfree@gmail.com>
2025-02-25 10:21:36 +0800

    2502. 设计内存分配器

--- src/main/java/org/hypnos/Allocator.java
+++ src/main/java/org/hypnos/Allocator.java
@@ -1,9 +1,6 @@
 package org.hypnos;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 2502. 设计内存分配器
@@ -12,57 +9,36 @@ public class Allocator {
 
     private int[] mem;
     private int n;
-    private Map<Integer, List<int[]>> map;
 
     public Allocator(int n) {
         this.n = n;
         mem = new int[n];
-        map = new HashMap<>();
     }
 
     public int allocate(int size, int mID) {
-        int idx = 0;
-        while (idx < n) {
-            if (mem[idx] == 0) {
-                int b = idx;
-                int cnt = 1;
-                if (size == 1) {
-                    map.computeIfAbsent(mID, k -> new ArrayList<>()).add(new int[]{b, size});
-                    for (int i = 0; i < size; ++i) {
-                        mem[b+i] = mID;
-                    }
-                    return b;
-                }
-                while (idx + 1 < n && mem[idx+1] == 0) {
-                    idx++;
-                    cnt++;
-                    if (cnt == size) {
-                        map.computeIfAbsent(mID, k -> new ArrayList<>()).add(new int[]{b, size});
-                        for (int i = 0; i < size; ++i) {
-                            mem[b+i] = mID;
-                        }
-                        return b;
-                    }
-                }
+        int free = 0;
+        for (int i = 0; i < n; ++i) {
+            if (mem[i] > 0) {
+                free = 0;
+                continue;
             }
-            idx++;
+            free++;
+            if (free == size) {
+                Arrays.fill(mem, i - size + 1, i + 1, mID);
+                return i - size + 1;
+            }
         }
         return -1;
     }
 
     public int freeMemory(int mID) {
-        List<int[]> idxs = map.get(mID);
-        if (idxs == null) return 0;
         int ans = 0;
-        for (int[] idx : idxs) {
-            int begin = idx[0];
-            int len = idx[1];
-            ans += len;
-            for (int i = 0; i < len; ++i) {
-                mem[begin + i] = 0;
+        for (int i = 0; i < n; ++i) {
+            if (mem[i] == mID) {
+                ans++;
+                mem[i] = 0;
             }
         }
-        map.remove(mID);
         return ans;
     }
 
