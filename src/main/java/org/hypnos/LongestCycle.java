e3890c9df2b1b4af5d15b19e53aa6483abee2b34
yuanrui01 <xdyrfree@gmail.com>
2025-03-29 09:21:59 +0800

    redo

--- src/main/java/org/hypnos/LongestCycle.java
+++ src/main/java/org/hypnos/LongestCycle.java
@@ -6,21 +6,22 @@ package org.hypnos;
 public class LongestCycle {
 
 	public int longestCycle(int[] edges) {
+		int ans = 0;
 		int n = edges.length;
-		int ans = -1;
-		int curTime = 1; // 当前时间
-		int[] visTime = new int[n]; // 首次访问 x 的时间
-		for (int i = 0; i < n; i++) {
+		int[] visTime = new int[n];
+		int curTime = 1;
+		for (int i = 0; i < n; ++i) {
 			int x = i;
-			int startTime = curTime; // 本轮循环的开始时间
-			while (x != -1 && visTime[x] == 0) { // 没有访问过 x
-				visTime[x] = curTime++; // 记录访问 x 的时间
-				x = edges[x]; // 访问下一个节点
+			int startTime = curTime;
+			while (x != -1 && visTime[x] >= 0) {
+				visTime[x] = curTime++;
+				x = edges[x];
 			}
-			if (x != -1 && visTime[x] >= startTime) { // x 在本轮循环中访问了两次，说明 x 在环上
-				ans = Math.max(ans, curTime - visTime[x]); // 前后两次访问 x 的时间差，即为环长
+			if (x != - 1 && visTime[x] >= startTime) {
+				ans = Math.max(ans, curTime - visTime[x]);
 			}
 		}
-		return ans; // 如果没有找到环，返回的是 ans 的初始值 -1
+
+		return ans;
 	}
 }
