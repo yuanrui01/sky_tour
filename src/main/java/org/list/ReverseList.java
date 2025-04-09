89a658a719b497acd2255d0fb627bdbbeca7bee5
yuanrui <xdyrfree@gmail.com>
2025-04-09 14:29:04 +0800

    206. 反转链表

--- src/main/java/org/list/ReverseList.java
+++ src/main/java/org/list/ReverseList.java
@@ -1,40 +1,22 @@
 package org.list;
 
+
 import org.common.ListNode;
 
-/**
- * 206. 反转链表
- */
+// 206. 反转链表
 public class ReverseList {
 
-	public ListNode reverseList(ListNode head) {
-		ListNode prev = null;
-		ListNode next = null;
-		while (head != null) {
-		    next = head.next;
-		    head.next = prev;
-		    prev = head;
-		    head = next;
-		}
-		return prev;
-	}
-	
-	public ListNode reverseList2(ListNode head) {
-		return head == null ? null : reverse(null, head);
-	}
-	
-	public ListNode reverse(ListNode parent, ListNode child) {
-		ListNode next = child.next;
-		child.next = parent;
-		return next == null ? child : reverse(child, next);
-	}
 
-	public static void main(String[] args) {
-		ListNode node1 = new ListNode(1);
-		ListNode node2 = new ListNode(2);
-		node1.next = node2;
-
-		ReverseList reverseList = new ReverseList();
-		reverseList.reverseList(node1);
-	}
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
 }
