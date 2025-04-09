8c62c3f80f19b68b572a777a08f42d003c98cf48
yuanrui <xdyrfree@gmail.com>
2025-04-09 15:40:53 +0800

    141. 环形链表

---
+++ src/main/java/org/list/HasCycle.java
@@ -0,0 +1,19 @@
+package org.list;
+
+import org.common.ListNode;
+
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
