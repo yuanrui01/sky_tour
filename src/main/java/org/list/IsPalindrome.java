8f82b13f8d93230aa04f8b8fb783f6826319b8c5
yuanrui <xdyrfree@gmail.com>
2025-04-09 14:45:18 +0800

    LCR 027. 回文链表

--- src/main/java/org/list/IsPalindrome.java
+++ src/main/java/org/list/IsPalindrome.java
@@ -2,27 +2,44 @@ package org.list;
 
 import org.common.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 /**
  * LCR 027. 回文链表
  */
 public class IsPalindrome {
 
+    // 先求出中间节点，然后对中间节点进行反转，之后顺序比较
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
-        int n = list.size();
-        for (int i = 0; i < n / 2; ++i) {
-            if (!Objects.equals(list.get(i), list.get(n - i - 1))) {
+        ListNode middleNode = findMiddleNode(head);
+        ListNode head2 = reverse(middleNode);
+        while (head2 != null) {
+            if (head2.val != head.val) {
                 return false;
             }
+            head2 = head2.next;
+            head = head.next;
         }
         return true;
+    }
+
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode node) {
+        if (node == null || node.next == null) return node;
+        ListNode pre = null;
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = pre;
+            pre = node;
+            node = next;
+        }
+        return pre;
     }
 }
