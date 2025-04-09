8ecd354dde2b968b55b55eb8659ac0d850ec8dbe
yuanrui <xdyrfree@gmail.com>
2025-04-09 17:02:38 +0800

    LRUCacheByLinkedHashMap

---
+++ src/main/java/org/design/LRUCacheByLinkedHashMap.java
@@ -0,0 +1,25 @@
+package org.design;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCacheByLinkedHashMap extends LinkedHashMap<Integer, Integer> {
+
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    private final int capacity;
+
+    public LRUCacheByLinkedHashMap(int capacity) {
+        super(capacity, DEFAULT_LOAD_FACTOR, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
