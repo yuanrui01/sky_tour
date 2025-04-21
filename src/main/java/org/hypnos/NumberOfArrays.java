330abc7f5b4722d51bfe985513279dd31b44d5e1
yuanrui01 <xdyrfree@gmail.com>
2025-04-21 19:51:15 +0800

    2145. 统计隐藏数组数目

--- src/main/java/org/hypnos/NumberOfArrays.java
+++ src/main/java/org/hypnos/NumberOfArrays.java
@@ -1,18 +1,17 @@
 package org.hypnos;
 
-
 /**
  * 2145. 统计隐藏数组数目
  */
 public class NumberOfArrays {
 
-    public int numberOfArrays(int[] differences, int lower, int upper) {
-        long s = 0, minS = 0, maxS = 0; // s[0] = 0
-        for (int d : differences) {
-            s += d;
-            minS = Math.min(minS, s);
-            maxS = Math.max(maxS, s);
-        }
-        return (int) Math.max(upper - lower - maxS + minS + 1, 0);
-    }
+	public int numberOfArrays(int[] differences, int lower, int upper) {
+		long sum = 0, min = 0, max = 0;
+		for (int d : differences) {
+			sum += d;
+			min = Math.min(min, sum);
+			max = Math.max(max, sum);
+		}
+		return Math.max((int)(upper - lower + min - max + 1), 0);
+	}
 }
