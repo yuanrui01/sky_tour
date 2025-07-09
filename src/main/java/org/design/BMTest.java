a783b8388b17c74c0512e6b7f445fdd94d179951
yuanrui01 <xdyrfree@gmail.com>
2025-07-10 03:00:07 +0800

    简陋bm字符串匹配

--- src/main/java/org/design/BMTest.java
+++ src/main/java/org/design/BMTest.java
@@ -2,6 +2,10 @@ package org.design;
 
 import java.util.Arrays;
 
+
+/**
+ * 简陋bm字符串匹配
+ */
 public class BMTest {
 
     private static final int SIZE = 256;
@@ -18,6 +22,9 @@ public class BMTest {
     public int bm(char[] a, int n, char[] b, int m) {
         int[] bc = new int[SIZE];
         generateBC(b, m, bc);
+		int[] suffix = new int[m];
+		boolean[] prefix = new boolean[m];
+		generateGS(b, m, suffix, prefix);
         int i = 0;
         while (i <= n - m) {
             int j;
@@ -27,8 +34,42 @@ public class BMTest {
             if (j < 0) {
                 return i;
             }
-            i = i + (j - bc[a[i+j]]);
+			int x = j - bc[a[i+j]];
+			int y = 0;
+			if (j < m - 1) {
+				y = moveByGS(j, m, suffix, prefix);
+			}
+            i = i + Math.max(x, y);
         }
         return -1;
     }
+
+	private void generateGS(char[] b, int m, int[] suffix, boolean[] prefix) {
+		for (int i = 0; i < m; ++i) {
+			suffix[i] = -1;
+			prefix[i] = false;
+		}
+		for (int i = 0; i < m - 1; ++i) {
+			int j = i;
+			int k = 0;
+			while (j >= 0 && b[j] == b[m-1-k]) {
+				j--;
+				k++;
+				suffix[k] = j + 1;
+			}
+			if (j == -1) prefix[k] = true;
+		}
+	}
+
+	private int moveByGS(int j, int m, int[] suffix, boolean[] prefix) {
+		int k = m - 1 - j;
+		if (suffix[k] != -1) return j - suffix[k] + 1;
+		for (int r = j + 2; r <= m - 1; ++r) {
+			if (prefix[m - r]) {
+				return r;
+			}
+		}
+		return m;
+	}
+
 }
